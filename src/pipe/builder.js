9e37f50bdb6c04eec86cb5474c61ec4fb2c8aeb6
Marc Liu <m@liu.pm>
2017-06-29 00:47:01 -0700

    Cleanup buildPipe function.

--- src/pipe/builder.js
+++ src/pipe/builder.js
@@ -15,7 +15,7 @@ export const FN_THROTTLE = 'throttle'
  * @param  {Array|String}   ...input String or array of names of dependencies.
  * @return {Pipeline}       Instance of this Pipeline.
  */
-export default function createPipe(pipeline, fn, input, output) {
+export default function createPipe(fn, input, output) {
   var fnType = typeof fn
 
   if ('number' === fnType) {
@@ -41,11 +41,11 @@ export default function createPipe(pipeline, fn, input, output) {
       default:
         return createInjectionPipe(fn, input, output)
     }
-  } else if (true === fn.instanceOfAlfaPipeline) {
-    fn = fn.toPipe(pipeline.getInjector())
+  } else if ('function' === fnType) {
+    return buildPipe(fn, input, output)
   }
 
-  return buildPipe(fn, input, output)
+  throw new Error(`Unsupported pipe function type "${fnType}".`)
 }
 
 function inputIsRequired(input, fnType) {
@@ -118,27 +118,47 @@ function createThrottlePipe(msec) {
   }
 }
 
-
+/**
+ * Create a pipe where the function is a dynamic value which will be injected
+ * from the store at execution time.
+ * 
+ * @param  {String}         name    Name of the pipe function
+ * @param  {Array|String}   input   String or array of names of inputs.
+ * @param  {Array|String}   output  String or array of names of outputs.
+ * @return {Object}                 Pipe definition object.
+ */
 function createInjectionPipe(name, input, output) {
   var pipe
+
+  /**
+   * A wrapper function to handle different types of pipe functions.
+   * It calls the original function and return the result if that is a function. 
+   * Or return the result directly for case like `boolean` value.
+   */
   var fn = function() {
     var ofn = pipe.ofn
     var ofnType = typeof ofn
     if ('function' === ofnType) {
-      // when it's a function call it with rest of the arguments
-      var result = ofn.apply(this, Array.from(arguments))
+      // Call it with the arguments passed in when it's a function.
+      // We call it with `0` to prevent some JS engines injecting the 
+      // default `this`.
+      var result = ofn.apply(0, Array.from(arguments))
+      // When the result is boolean we will need to consider if it's a `not` 
+      // pipe and alter the value based on that.
       if ('boolean' === typeof result)
         return pipe.not ? !result : result
       else
         return result
     } else if ('boolean' === ofnType) {
-      // directly return the value when it is a boolean for flow control
+      // Directly return the value when it is a boolean for flow control.
       return pipe.not ? !ofn : ofn
     } else if (true === pipe.optional && 'undefined' === ofnType) {
-      // Optional pipe which pipe function can not be found.
+      // Optional pipe which its function can not be found.
       // Return true to ignore and go next.
       return true
     } else {
+      // Throw an exception when the original function is not something
+      // we understand.
       throw new Error('Dependency `' + name + '` is not a function.')
     }
   }
@@ -146,58 +166,22 @@ function createInjectionPipe(name, input, output) {
   // Build the pipe.
   pipe = buildPipe(fn, input, output)
 
+  // It's a `not` pipe if the pipe name is started with `!`.
+  // Although the actual function name is the value without the exclamation mark.
   if (/^!/.test(name)) {
     pipe.not = true
     name = name.slice(1)
   }
 
+  // It's an `optional` pipe if the name is ended with `?`.
+  // The actual function name is the value without the question mark.
   if (/\?$/.test(name)) {
-    // Optional pipe if function name ends with question mark.
     pipe.optional = true
-    // Remove the trailing question mark.
     name = name.slice(0, -1)
   }
 
-  if ('set' === name) {
-    // a `set` pipe function with customized position based output
-    var _output = pipe.output
-    if (_output && (!input || 'number' === typeof input || isArray(input))) {
-      pipe.fn = function() {
-        var args
-        var positioned
-        if (1 === arguments.length
-          && (!input || 1 === input)
-          && 'object' === typeof arguments[0]
-          && !isArray(arguments[0])) {
-          args = arguments[0]
-        } else {
-          args = arguments
-          positioned = true
-        }
-
-        var result = {}
-        foreach(_output, function(supplyName, idx) {
-          result[supplyName] = positioned ? args[idx] : args[supplyName]
-        })
-        return result
-      }
-    } else if ('object' === typeof input && !isArray(input)) {
-      // mapped dependencies by position of output
-      pipe.input = null
-      pipe.fn = function() {
-        var args = arguments
-        var result = {}
-        // `value` is index of arguments
-        // `key` is name of supply
-        foreach(input, function(value, key) {
-          result[key] = args[value]
-        })
-        return result
-      }
-    }
-  }
-
-  // Set the original function name to the pipe object for later dependency discovery.
+  // Set the original function name to the pipe object 
+  // for later dependency discovery.
   pipe.fnName = name
   return pipe
 }
@@ -205,69 +189,65 @@ function createInjectionPipe(name, input, output) {
 /**
  * The actual function for building a pipe.
  *
- * @param  {Function} fn
- *         - Function: The pipe function
- *         - String: Name of function which could be found in dependencies.
- *         - Number: Number of miliseconds to throttle the pipeline.
- *         - Array: A array which contains both `fn` (first) and `deps` (rest).
- * @param  {Array|String}   deps... String or array of names of dependencies.
- * @return {Object}         An object contains dependencies injected function and deps.
+ * @param  {Function}       fn      The pipe function
+ * @param  {Array|String}   input   String or array of names of inputs.
+ * @param  {Array|String}   output  String or array of names of outputs.
+ * @return {Object}                 Pipe definition object.
  */
-function buildPipe(fn, deps, supplies) {
-  if ('function' !== typeof fn)
-    throw new Error('fn should be a function, got ' + typeof fn)
-
-  var depsType = typeof deps
-  if ('string' === depsType || 'number' === depsType)
-    deps = [deps]
-
-  if ('string' === typeof supplies)
-    supplies = [supplies]
-
-  if (deps && !isArray(deps)) {
-    if ('object' === depsType)
-      deps = [deps]
-    else
-      throw new Error('`dependencies` should be either string, array or object of dependency names if present')
-  }
-
-  if (supplies && !isArray(supplies))
-    throw new Error('supplies should be either string or array of dependency names if present')
-
-  // If first dependency is a number it tells us the number of original function
-  // arguments we need to pass for calling the pipe function.
-  if (deps) {
-    var firstDep = deps[0]
-    if ('number' === typeof firstDep && 0 < firstDep) {
-      deps[0] = null
-      while (1 < firstDep) {
-        deps.unshift(null)
-        firstDep--
-      }
-    }
-  }
-
-  // Detect any mapped supplies. Use the format `theOriginalName:theNewName` in
-  // `supplies` array would cause the `setDep('theOriginalName', obj)` to actually
-  // set the dependency equals calling `setDep('theNewName', obj)` hence mapped
-  // the dependency name.
-  var setDepMap
-  foreach(supplies, function(supply) {
-    if (/:/.test(supply)) {
-      setDepMap = setDepMap || {}
-      var mapping = supply.split(':')
-      setDepMap[mapping[0]] = mapping[1]
-    }
-  })
+function buildPipe(fn, input, output) {
+  input = normalizeInput(input)
+  output = normalizeOutput(output)
 
   // Return pipe object with function and its metadata.
   return {
-    fn: fn, // Original or wrapped function, should be never changed.
+    fn: fn, // Original or wrapped function, should never be changed.
     ofn: fn, // The ofn property might be changed during pipeline execution for
     // loading/generating pipe functions dynamically.
-    deps: deps,
-    fnName: getFnName(fn),
-    supplies: supplies,
-    setDepMap: setDepMap
+    input: input,
+    output: output.output,
+    outputMap: output.outputMap
+  }
+}
+
+function normalizeInput(input) {
+  var inputType = typeof input
+  if ('string' === inputType)
+    input = [input]
+
+  if (input && !Array.isArray(input)) {
+    if ('object' === inputType)
+      input = [input]
+    else
+      throw new Error('`input` should be either string, array or object of dependency maps when presents')
+  }
+
+  return input
+}
+
+function normalizeOutput(output) {
+  if ('string' === typeof output)
+    output = [output]
+
+  if (output && !Array.isArray(output))
+    throw new Error('`output` should be either string or array of output names when presents')
+
+  // Detect any mapped output. Use the format `theOriginalName:theNewName` in
+  // `output` array will map the output `theOriginalName` to `theNewName`.
+  var i = 0
+  var len = output.length
+  var outputMap
+
+  while (i < len) {
+    var item = output[i]
+    if (/:/.test(item)) {
+      outputMap = outputMap || {}
+      var mapping = item.split(':')
+      outputMap[mapping[0]] = mapping[1]
+    }
+  }
+
+  return {
+    output: output,
+    outputMap: outputMap
   }
 }
