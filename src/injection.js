c05a8acf56a0c33b6fec82c565da66b19beb8498
Marc Liu <m@liu.pm>
2017-07-05 00:12:59 -0700

    Use Object.assign and set function name for wrapper function.

--- src/injection.js
+++ src/injection.js
@@ -33,22 +33,23 @@ function isReactComponent(Component) {
 }
 
 
-function createAlfaProvidedFunction(store, Func, keys) {
-  function AlfaProvidedFunction(props, context) {
-    // Props passed in directly to constructor has higher priority than keys
-    // injected from the store.
-    // Note: We can certainly define and get a non-global store instance from 
-    // context.  But, the question is - what are the benefits?
-    props = {
-      ...store.get(keys),
-      ...props
+function createAlfaProvidedFunction(store, WrappedFunction, keys) {
+  // Keep the name of the orginal function which makes debugging logs easier
+  // to understand.
+  var funcName = WrappedFunction.name || 'AlfaProvidedFunction'
+
+  return ({
+    [funcName]: function(props, context, updater) {
+      // Props passed in directly to constructor has higher priority than keys
+      // injected from the store.
+      // Note: We can certainly define and get a non-global store instance from 
+      // context.  But, the question is - what are the benefits?
+      props = Object.assign(store.get(keys), props || {})
+
+      // Call the original function.
+      return WrappedFunction(props, context, updater)
     }
-
-    // Call the original function.
-    return Func(props, context)
-  }
-
-  return AlfaProvidedFunction
+  })[funcName]
 }
 
 function createAlfaProvidedComponent(store, Component, keys) {
