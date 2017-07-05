229016ab60886bc31e5954ea09b83b44d54937d8
Marc Liu <m@liu.pm>
2017-07-04 17:48:10 -0700

    Export alfa and toplevel functions.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-export { action, provide, subscribe, createAlfa } from './alfa'
+import alfa from './alfa'
+
+export default alfa
+export const get = alfa.get
+export const set = alfa.set
+export const action = alfa.action
+export const provide = alfa.provide
+export const subscribe = alfa.subscribe
+export const createAlfa = alfa.createAlfa
