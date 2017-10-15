7320fa6dde7bf9ad8e1dee4b42c2e93004130e80
Marc Liu <m@liu.pm>
2017-10-14 21:42:22 -0700

    JSDOM bootstrap file for testing in nodejs.

---
+++ test/setup.js
@@ -0,0 +1,31 @@
+
+if ('unit' === process.env.TEST_ENV)
+  setup()
+
+var initialized = false
+
+function setup() {
+  if (true === initialized)
+    return
+
+  initialized = true
+
+  const {JSDOM} = require('jsdom')
+
+  const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
+  const {window} = jsdom
+
+  function copyProps(src, target) {
+    const props = Object.getOwnPropertyNames(src)
+      .filter(prop => 'undefined' === typeof target[prop])
+      .map(prop => Object.getOwnPropertyDescriptor(src, prop))
+    Object.defineProperties(target, props)
+  }
+
+  global.window = window
+  global.document = window.document
+  global.navigator = {
+    userAgent: 'node.js',
+  }
+  copyProps(window, global)
+}
