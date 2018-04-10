39416912bc92efe64095b34907be038a371e14e7
Marc <m@liu.pm>
2018-04-10 15:17:45 -0700

    Remove global alfa instance & store.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
-import createStore from './store'
-import { createInjector } from './injection'
-
 /**
- * Export function for creating a new alfa instance.
- * @return {Function}
+ * Export toplevel APIs.
  */
-export function createAlfa() {
-  const alfa = {}
-  const store = createStore()
-
-  alfa.provide = createInjector(store, 'provide')
-  alfa.subscribe = createInjector(store, 'subscribe')
-
-  return alfa
-}
-
-/**
- * Default global instance of alfa.
- * @type {Function}
- */
-const alfa = createAlfa()
-
-/**
- * Export default
- */
-export default alfa
-
-/**
- * Export other toplevel APIs.
- */
-export const provide = alfa.provide
-export const subscribe = alfa.subscribe
 export { default as app } from './app'
+export { provide, subscribe } from './injection'
 export { default as createStore } from './store'
--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,9 @@
 import test from 'tape'
-import alfa, { provide, subscribe, createAlfa, createStore } from '../src'
+import { provide, subscribe, createStore } from '../src'
 
 test('Public alfa api exists', t => {
-  t.is(typeof alfa, 'object')
   t.is(typeof provide, 'function')
   t.is(typeof subscribe, 'function')
-  t.is(typeof createAlfa, 'function')
   t.is(typeof createStore, 'function')
-  t.end()
-})
-
-test('alfa() returns a new alfa instance', t => {
-  var newAlfa = createAlfa()
-  t.is(typeof newAlfa, 'object')
-  t.is(typeof newAlfa.provide, 'function')
-  t.is(typeof newAlfa.subscribe, 'function')
   t.end()
 })
